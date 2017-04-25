4c7f874cb11e18cc4710319a8cc24fc35f883172
Dzov <amelie.haladjian@gmail.com>
2017-04-25 17:18:27 +0200

    dropdown and filter by genre

--- views/view.php
+++ views/view.php
@@ -15,12 +15,22 @@
 
   <main class="flexColumn" ng-controller="MainCtrl">
 
-    <button class="randomBtn">Find a random show !</button>
-    <ul class="btnList flex justifyAround">
-      <li class="button" ng-click='myFilter = {Genre : "Drama"}'>Genre</li>
-      <li class="button">Year</li>
-      <li class="button" ng-click='myFilter = {Rating : "9.5"}'>Highest Ratings</li>
-    </ul>
+    <button class="randomBtn" ng-click=''>Find a random show !</button>
+    <div class="btnList flex justifyAround">
+      <div class="dropDownMenu">
+        <button class="button genreBtn">Genre</button>
+        <ul class="dropContent">
+        <li ng-click='myFilter = {Genre : "Adventure"}'>Adventure</li>
+          <li ng-click='myFilter = {Genre : "Animation"}'>Animation</li>
+          <li ng-click='myFilter = {Genre : "Comedy"}'>Comedy</li>
+          <li ng-click='myFilter = {Genre : "Crime"}'>Crime</li>
+          <li ng-click='myFilter = {Genre : "Drama"}'>Drama</li>
+          <li ng-click='myFilter = {Genre : "Sci-Fi"}'>Sci-Fi</li>
+        </ul>
+      </div>
+      <button class="button" ng-click='myFilter = {Rating : "9.5"}'>Highest Ratings</button>
+      <button class="button" ng-click='myFilter = {Genre : ""} '>All shows</button>
+    </div>
     <h2 class="selfCenter">Here are a few suggestions to choose from</h2>
 
     <!-- TV show cards section -->
